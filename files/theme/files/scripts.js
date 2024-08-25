0baa472c72ece930006a53356a09b462ee379207
Peter Yehl <yehl.peter@gmail.com>
2024-08-25 13:42:44 -0500

    span

--- files/theme/files/scripts.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var hamburger = document.querySelector('.hamburger');
-    var navWrap = document.querySelector('.nav-wrap');
-
-    hamburger.addEventListener('click', function (event) {
-        event.preventDefault();
-        navWrap.classList.toggle('show');
-    });
-});
